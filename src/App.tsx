f684a6624b508af871328b871720377a450a7fc3
Zach <104291766+zachary-o@users.noreply.github.com>
2023-07-03 22:34:49 +0700

    hangman project upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,99 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+
+require("./styles.css");
 
 function App() {
+  const [step, setStep] = useState(0);
+  const [randomWord, setRandomWord] = useState("dog");
+  const [indexes, setIndexes] = useState<number[]>([]);
+  const [pressedLetters, setPressedLetters] = useState<string[]>([]);
+
+  const alphabet: string[] = "abcdefghijklmnopqrstuvwxyz".split("");
+  const words: string[] = ["book", "chair", "table"];
+
+  useEffect(() => {
+    const randomWordGenerator = () => {
+      const randomWord: string = words[Math.floor(Math.random() * 3)];
+      setRandomWord(randomWord);
+    };
+    randomWordGenerator();
+  }, []);
+
+  useEffect(() => {
+    const endGame = randomWord.length === indexes.length;
+    if (endGame) {
+      alert(`Congratulations! You guessed the word ${randomWord}`);
+    }
+  }, [indexes]);
+
+  const checkLetter = (letter: string) => {
+    if (pressedLetters.includes(letter)) {
+      return;
+    }
+    setPressedLetters((prevLetters) => [...prevLetters, letter]);
+
+    if (randomWord.includes(letter)) {
+      let indexesArr: number[] = [...indexes];
+      for (let i = 0; i < randomWord.length; i++) {
+        if (randomWord[i] === letter && !indexes.includes(i)) {
+          indexesArr.push(i);
+        }
+      }
+      setIndexes(indexesArr);
+    } else if (!randomWord.includes(letter)) {
+      setStep((prevStep) => prevStep + 1);
+    }
+  };
+
+  const letterStyling = (letter: string): string => {
+    const found = indexes.find((i) => {
+      return randomWord[i] === letter;
+    });
+    if (found !== undefined) {
+      return "letter";
+    } else {
+      return "letter-hidden";
+    }
+  };
+
+  console.log(indexes);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="gallows">
+        <img src={`./images/${step}.png`} alt="" className="gallows-image" />
+      </div>
+      <div className="word">
+        {randomWord.split("").map((letter, index) => (
+          <p key={index} className={letterStyling(letter)}>
+            {letter}
+          </p>
+        ))}
+      </div>
+      <div className="underscores">
+        {Array(randomWord.length)
+          .fill("_")
+          .map((underscore, index) => (
+            <p key={index} className="underscore">
+              {underscore}
+            </p>
+          ))}
+      </div>
+      <div className="alphabet">
+        {alphabet.map((letter, index) => (
+          <button
+            key={index}
+            className={
+              pressedLetters.includes(letter)
+                ? "alphabet-button-disabled"
+                : "alphabet-button"
+            }
+            onClick={() => checkLetter(letter)}
+          >
+            {letter}
+          </button>
+        ))}
+      </div>
     </div>
   );
 }
